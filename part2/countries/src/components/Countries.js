fd53feba3a0a56f17e0e18bb97d8f745b42e9fab
Ross Middleton <rossmidd@gmail.com>
2022-01-13 13:20:56 +0000

    Completed exercise 2.13*

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DetailedCountry from './DetailedCountry'
 
 const Countries = ({ filter, countries }) => {
+  const [countriesToShow, newCountriesToShow] = useState([])
   const filteredCountries = countries.filter((country) =>
     country.name.common.toLowerCase().includes(filter.toLowerCase())
   )
+
+  const handleButtonPress = (cca2) => {
+    newCountriesToShow(cca2)
+    console.log(`countriesToShow`, countriesToShow)
+  }
 
   if (filteredCountries.length > 10) {
     return <div>Too many matches, specify another filter</div>
@@ -14,7 +20,15 @@ const Countries = ({ filter, countries }) => {
     return (
       <ul>
         {filteredCountries.map((country) => (
-          <li key={country.cca2}>{country.name.common} </li>
+          <li key={country.cca2}>
+            {country.name.common}{' '}
+            <button onClick={() => handleButtonPress(country.cca2)}>
+              show
+            </button>
+            {countriesToShow === country.cca2 ? (
+              <DetailedCountry country={country} />
+            ) : null}
+          </li>
         ))}
       </ul>
     )
